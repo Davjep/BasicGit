523cf1870b941034dfb2d22ccb61729bae6332d0
Jepsson <davjep-0@student.ltu.se>
2021-03-09 15:50:15 +0100

    public int added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class Main {
     public String hej = "Hej";
-    
+    public int awesomeness = 10;
+
     public static void main(String[] args) {
 	// write your code here
-
+        
     }
 }
